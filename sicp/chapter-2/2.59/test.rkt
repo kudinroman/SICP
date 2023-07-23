095fce3b879e0ebf735ce678af2f8206c3d825da
Roman Kudin <kudinroman82@gmail.com>
2023-07-24 00:52:07 +0300

    2.59

---
+++ sicp/chapter-2/2.59/test.rkt
@@ -0,0 +1,19 @@
+#lang racket/base
+
+(require rackunit "2.59.rkt")
+
+(define my-set (list 2 4 'asd))
+(define adjoin-my-set (list 'asdf 2 4 'asd))
+(check-equal? (element-of-set? 2 my-set) #t)
+(check-equal? (element-of-set? 3 my-set) #f)
+(check-equal? (element-of-set? 'asd my-set) #t)
+
+(check-equal? (adjoin-set 'asd my-set) my-set)
+(check-equal? (adjoin-set 'asdf my-set) adjoin-my-set)
+
+(check-equal? (intersection-set my-set adjoin-my-set) my-set)
+
+(check-equal? (union-set adjoin-my-set my-set) adjoin-my-set)
+(check-equal? (union-set '(a b c) '(a b d)) '(c a b d))
+(check-equal? (union-set '(a b c) '(d e f)) '(a b c d e f))
+
